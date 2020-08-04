4b559e5981341802a2cae09648959b79e58b316b
Jesse Szepieniec <jessems@gmail.com>
2020-08-04 08:27:12 +0200

    Added substack subscribe

--- src/components/Forms/Subscribe.js
+++ src/components/Forms/Subscribe.js
@@ -71,129 +71,132 @@ class SignUp extends React.Component {
     const successful = response && response.status === 'success'
 
     return (
-      <div>
-        {!successful && (
-          <h2
-            css={css`
-              margin-bottom: ${rhythm(1)};
-              margin-top: 0;
-            `}
-          >
-            Join the Newsletter
-          </h2>
-        )}
-
-        <Formik
-          initialValues={{
-            email_address: '',
-            first_name: '',
-          }}
-          validationSchema={SubscribeSchema}
-          onSubmit={values => this.handleSubmit(values)}
-          render={({ errors, touched, isSubmitting }) => (
-            <>
-              {!successful && (
-                <Form
-                  css={css`
-                    display: flex;
-                    align-items: flex-end;
-                    button {
-                      margin-left: 10px;
-                    }
-                    .field-error {
-                      display: block;
-                      color: ${theme.colors.red};
-                      font-size: 80%;
-                    }
-                    input,
-                    label {
-                      width: 100%;
-                    }
-                    ${bpMaxSM} {
-                      flex-direction: column;
-                      align-items: flex-start;
-                      width: auto;
-                      label,
-                      input {
-                        margin: 5px 0 0 0 !important;
-                        width: 100%;
-                        display: flex;
-                        flex-direction: column;
-                      }
-                      button {
-                        margin: 20px 0 0 0;
-                      }
-                    }
-                  `}
-                >
-                  <label htmlFor="first_name">
-                    <div
-                      css={css`
-                        display: flex;
-                        justify-content: space-between;
-                        align-items: flex-end;
-                      `}
-                    >
-                      First Name
-                      <ErrorMessage
-                        name="first_name"
-                        component="span"
-                        className="field-error"
-                      />
-                    </div>
-                    <Field
-                      aria-label="your first name"
-                      aria-required="false"
-                      name="first_name"
-                      placeholder="Jane"
-                      type="text"
-                    />
-                  </label>
-                  <label
-                    htmlFor="email"
-                    css={css`
-                      margin-left: 10px;
-                    `}
-                  >
-                    <div
-                      css={css`
-                        display: flex;
-                        justify-content: space-between;
-                        align-items: flex-end;
-                      `}
-                    >
-                      Email
-                      <ErrorMessage
-                        name="email_address"
-                        component="span"
-                        className="field-error"
-                      />
-                    </div>
-                    <Field
-                      aria-label="your email address"
-                      aria-required="true"
-                      name="email_address"
-                      placeholder="jane@acme.com"
-                      type="email"
-                    />
-                  </label>
-                  <button
-                    data-element="submit"
-                    type="submit"
-                    disabled={isSubmitting}
-                  >
-                    {!isSubmitting && 'Submit'}
-                    {isSubmitting && 'Submitting...'}
-                  </button>
-                </Form>
-              )}
-              {submitted &&
-                !isSubmitting && <PostSubmissionMessage response={response} />}
-              {errorMessage && <div>{errorMessage}</div>}
-            </>
-          )}
-        />
+      <div css={css`width: '100%'; display: flex; justify-content: center`}>
+        <iframe src="https://jessems.substack.com/embed" width="480" height="320" frameborder="0" scrolling="no"></iframe>
       </div>
+      // <div>
+      //   {!successful && (
+      //     <h2
+      //       css={css`
+      //         margin-bottom: ${rhythm(1)};
+      //         margin-top: 0;
+      //       `}
+      //     >
+      //       Join the Newsletter
+      //     </h2>
+      //   )}
+
+      //   <Formik
+      //     initialValues={{
+      //       email_address: '',
+      //       first_name: '',
+      //     }}
+      //     validationSchema={SubscribeSchema}
+      //     onSubmit={values => this.handleSubmit(values)}
+      //     render={({ errors, touched, isSubmitting }) => (
+      //       <>
+      //         {!successful && (
+      //           <Form
+      //             css={css`
+      //               display: flex;
+      //               align-items: flex-end;
+      //               button {
+      //                 margin-left: 10px;
+      //               }
+      //               .field-error {
+      //                 display: block;
+      //                 color: ${theme.colors.red};
+      //                 font-size: 80%;
+      //               }
+      //               input,
+      //               label {
+      //                 width: 100%;
+      //               }
+      //               ${bpMaxSM} {
+      //                 flex-direction: column;
+      //                 align-items: flex-start;
+      //                 width: auto;
+      //                 label,
+      //                 input {
+      //                   margin: 5px 0 0 0 !important;
+      //                   width: 100%;
+      //                   display: flex;
+      //                   flex-direction: column;
+      //                 }
+      //                 button {
+      //                   margin: 20px 0 0 0;
+      //                 }
+      //               }
+      //             `}
+      //           >
+      //             <label htmlFor="first_name">
+      //               <div
+      //                 css={css`
+      //                   display: flex;
+      //                   justify-content: space-between;
+      //                   align-items: flex-end;
+      //                 `}
+      //               >
+      //                 First Name
+      //                 <ErrorMessage
+      //                   name="first_name"
+      //                   component="span"
+      //                   className="field-error"
+      //                 />
+      //               </div>
+      //               <Field
+      //                 aria-label="your first name"
+      //                 aria-required="false"
+      //                 name="first_name"
+      //                 placeholder="Jane"
+      //                 type="text"
+      //               />
+      //             </label>
+      //             <label
+      //               htmlFor="email"
+      //               css={css`
+      //                 margin-left: 10px;
+      //               `}
+      //             >
+      //               <div
+      //                 css={css`
+      //                   display: flex;
+      //                   justify-content: space-between;
+      //                   align-items: flex-end;
+      //                 `}
+      //               >
+      //                 Email
+      //                 <ErrorMessage
+      //                   name="email_address"
+      //                   component="span"
+      //                   className="field-error"
+      //                 />
+      //               </div>
+      //               <Field
+      //                 aria-label="your email address"
+      //                 aria-required="true"
+      //                 name="email_address"
+      //                 placeholder="jane@acme.com"
+      //                 type="email"
+      //               />
+      //             </label>
+      //             <button
+      //               data-element="submit"
+      //               type="submit"
+      //               disabled={isSubmitting}
+      //             >
+      //               {!isSubmitting && 'Submit'}
+      //               {isSubmitting && 'Submitting...'}
+      //             </button>
+      //           </Form>
+      //         )}
+      //         {submitted &&
+      //           !isSubmitting && <PostSubmissionMessage response={response} />}
+      //         {errorMessage && <div>{errorMessage}</div>}
+      //       </>
+      //     )}
+      //   />
+      // </div>
     )
   }
 }
