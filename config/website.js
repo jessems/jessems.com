ecb2c609c83eb5902ef87bc3e14ca0f1c02b7013
Jesse Szepieniec <jessems@gmail.com>
2020-11-09 09:36:20 +0100

    Updated site description

--- config/website.js
+++ config/website.js
@@ -6,7 +6,7 @@ module.exports = {
   siteUrl: 'https://jessems.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'This is where they post things!',
+  siteDescription: 'Jesse M. Szepieniec',
   author: 'Jesse', // Author for schemaORGJSONLD
   organization: 'Jesse MS',
 
